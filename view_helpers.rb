f8ff1e7f2ba9a7a5321507c93f6f108f5b60f763
NaiTing Liu <sorarize@gmail.com>
2014-08-31 23:51:35 +0800

    Added og and partialized

---
+++ view_helpers.rb
@@ -0,0 +1,18 @@
+module ViewHelpers
+
+  # yaml data
+  def data
+    d||=YAML.load_file(File.join(File.dirname(__FILE__), 'data.yml'))
+    return d
+  end
+
+  # nav li active link
+  def nav_link_to(name, href, options={})
+    here = request.path.gsub('index.html', '')
+    if href == here
+      active = "active"
+    end
+    content_tag(:li, link_to(name, href, :class => active) )
+  end
+
+end
